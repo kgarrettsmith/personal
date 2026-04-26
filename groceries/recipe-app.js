11661255836f9bc519a5aea46c5325c564907a2e
Garrett <kgsmith902@gmail.com>
2026-04-25 21:52:43 -0400

    Update recipe-app.js

--- groceries/recipe-app.js
+++ groceries/recipe-app.js
@@ -11,10 +11,10 @@ let checkedItems = JSON.parse(localStorage.getItem("checkedItems") || "{}");
 document.body.dataset.theme = selectedTheme;
 
 function getHeadshot() {
-  if (selectedTheme === "lost") return "images/lost-headshot.png";
-  if (selectedTheme === "psych") return "images/psych-headshot.png";
-  if (selectedTheme === "survivor") return "images/survivor-headshot.png";
-  return "images/headshot.png";
+  if (selectedTheme === "lost") return "images/headshots/lost-headshot.png";
+  if (selectedTheme === "psych") return "images/headshots/psych-headshot.png";
+  if (selectedTheme === "survivor") return "images/headshots/survivor-headshot.png";
+  return "images/headshots/headshot.png";
 }
 
 
